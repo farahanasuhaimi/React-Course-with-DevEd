4e15240ad6d5d7d17a6805a9fc981266f15d5080
farahanasuhaimi <farahanams@gmail.com>
2021-02-12 00:36:19 +0800

    styled component

--- Chapter 1/react-starter/src/components/Tweet.js
+++ Chapter 1/react-starter/src/components/Tweet.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import styled from 'styled-components'
 
 const Tweet = ({name, tweet, tweets, setTweets}) => {
     const deleteTweet = () => {
         setTweets(tweets.filter((state) => state.id !== tweet.id))
     }
     return (
-        <div className='Tweet'>
+        <TweetStyled className='Tweet'>
             <h2>Name: {name}</h2>
             <h3>Tweet: {tweet.message}</h3>
             <button onClick={deleteTweet} >Delete</button>
             <button>Like</button>
-        </div>
+        </TweetStyled>
     )
 }
 
+const TweetStyled = styled.div`
+background-color: red;
+border-radius: 3px;
+`
 export default Tweet;
