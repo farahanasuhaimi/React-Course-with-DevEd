9f240486563174a0b5e8e1f8abf4455d88b81ac2
farahanasuhaimi <farahanams@gmail.com>
2021-02-13 22:57:55 +0800

    added songs for music player.

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,9 +1,14 @@
+import React, {useState} from "react"
 import Player from './components/Player'
 import Song from './components/Song'
 
 import './styles/app.scss'
 
+import data from "./data"
+
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
   return (
     <div className="App">
         <Song />
