b252f7da801d77ef7b3ced1c9cdf59c8418573fb
farahanasuhaimi <farahanams@gmail.com>
2021-03-11 23:48:43 +0800

    added new components

--- music-player/src/components/Library.js
+++ music-player/src/components/Library.js
@@ -1,10 +1,15 @@
 import react from "react";
+import LibrarySong from "./LibrarySong";
 
 const Library = () => {
   return (
     <div className="library">
       <h2>Library</h2>
-      <div className="library-songs"></div>
+      <div className="library-songs">
+        <LibrarySong />
+      </div>
     </div>
   );
 };
+
+export default Library;
